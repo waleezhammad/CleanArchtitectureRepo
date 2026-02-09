b0504d48135005ea45b0a76e3daef9d97a4a19cb
Waleed Hammad <eng_walid2007@hotmail.com>
2026-02-10 00:11:20 +0300

    add class with properties

---
+++ src/IntegrationService.Domain/Enums/file.cs
@@ -0,0 +1,23 @@
+class file
+{
+    public string Name { get; set; }
+    public long Size { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+
+    public file (string name, long size, DateTime createdDate, DateTime modifiedDate)
+    {
+        Name = name;
+        Size = size;
+        CreatedDate = createdDate;
+        ModifiedDate = modifiedDate;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Size: {Size} bytes");
+        Console.WriteLine($"Created Date: {CreatedDate}");
+        Console.WriteLine($"Modified Date: {ModifiedDate}");
+    }
+}
